6d282885e46fc2d8641d1b1987167d61f9c464c5
cedretaber <cedretaber@gmail.com>
2020-04-14 02:18:53 +0900

    Solve DPL_1_E.

---
+++ dpl/DPL_1_E/hen.d
@@ -0,0 +1,8 @@
+import std.stdio, std.algorithm, std.conv, std.array, std.string, std.math, std.typecons, std.numeric;
+
+void main()
+{
+    auto s1 = readln.chomp;
+    auto s2 = readln.chomp;
+    writeln(levenshteinDistance(s1, s2));
+}
